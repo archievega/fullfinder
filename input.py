e053fcd1ed5a236c9ba746c51be8b71800fe5dd0
em1tao <arslan.semenoff@gmail.com>
2020-08-15 04:47:10 +0500

    Update

---
+++ input.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# em1tao
+import easygui
+
+print("Choose image file")
+
+while True:
+    screenshot = easygui.fileopenbox()
+    if screenshot[-3::] == "png" or screenshot[-3::] == "jpg":
+        print(f"You choose '{screenshot}'")
+        break
+    else:
+        print("Choose image file")
+
+print("Enter some tags (for example hair color) \n Type '.' to stop")
+tags = []
+tag = ""
+
+while True:
+    tag = input(":")
+    if tag == ".":
+        if len(tags) >= 1:
+            break
+        else:
+            print("Write at least 1 tag")
+            continue
+    else:
+        tags.append(tag)
